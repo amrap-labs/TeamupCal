1611b0737dcab7eec6cff5334512a38dd6c9a35c
Merrick Sapsford <merrick@sapsford.tech>
2017-11-14 21:08:59 +0100

    Add project

---
+++ Sources/TeamupCal/TeamupCal.h
@@ -0,0 +1,19 @@
+//
+//  TeamupCal.h
+//  TeamupCal
+//
+//  Created by Merrick Sapsford on 14/11/2017.
+//  Copyright © 2017 AMRAP Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TeamupCal.
+FOUNDATION_EXPORT double TeamupCalVersionNumber;
+
+//! Project version string for TeamupCal.
+FOUNDATION_EXPORT const unsigned char TeamupCalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TeamupCal/PublicHeader.h>
+
+
