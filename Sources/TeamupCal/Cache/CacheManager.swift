d99c4ef75371ddaf3e8534352345bf9d707edeef
Merrick Sapsford <merrick@sapsford.tech>
2017-11-15 21:26:10 +0100

    Add CacheManager

---
+++ Sources/TeamupCal/Cache/CacheManager.swift
@@ -0,0 +1,13 @@
+//
+//  CacheManager.swift
+//  TeamupCal
+//
+//  Created by Merrick Sapsford on 15/11/2017.
+//  Copyright © 2017 AMRAP Labs. All rights reserved.
+//
+
+import Foundation
+
+internal class CacheManager {
+    
+}
